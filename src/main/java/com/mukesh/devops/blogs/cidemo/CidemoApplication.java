4e4a0fcc273e5f54cafdbfcde144d5edab1c01da
Vivekanandan <mukesh.kuduvavivekanandan@cgi.com>
2023-09-06 16:07:03 -0400

    Introducing GET method hello endpoint

--- src/main/java/com/mukesh/devops/blogs/cidemo/CidemoApplication.java
+++ src/main/java/com/mukesh/devops/blogs/cidemo/CidemoApplication.java
@@ -2,6 +2,8 @@ package com.mukesh.devops.blogs.cidemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class CidemoApplication {
@@ -10,4 +12,28 @@ public class CidemoApplication {
 		SpringApplication.run(CidemoApplication.class, args);
 	}
 
+}
+
+@RestController
+class HelloWorldController {
+    @GetMapping("/hello")
+    public Message helloWorld() {
+        return new Message("Hello World");
+    }
+}
+
+class Message {
+    private String message;
+
+    public Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
